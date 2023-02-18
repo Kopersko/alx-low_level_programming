304b128b7b4ff63c61ca9b8d272a11c1400f065f
Kopersko <rayenroni76@gmail.com>
2023-02-18 15:22:15 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ *main - entry point
+ *description:Random number
+ *Return: returns 0
 /* betty style doc for function main goes there */
 int main(void)
 {
