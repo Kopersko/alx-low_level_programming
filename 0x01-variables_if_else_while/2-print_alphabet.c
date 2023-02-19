7f7793c2dbe4cbfe6841ee532ad1454953316b93
Kopersko <rayenroni76@gmail.com>
2023-02-18 18:52:57 -0800

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-        int i = 0;
-        while(alphabet[i] != '\0')
-	{
-        putchar(alphabet[i]);
-        i++;
-        }
-	putchar('\n');
-	return 0;
+	char letter = 'a';
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+	return (0);
 }
